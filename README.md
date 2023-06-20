94c1c44289fd165d540ab1bc8e838dd8773f77bf
tarunjainsagar <tarun@mobisy.com>
2023-06-20 15:56:31 +0530

    init: rust examples - variables

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rust-programming
+
 Implemented assignments to learn rust programming language
+
+1 - grep-clone: create an cli command to replicate the behaviour of linux grep command.
+
+2 - p2p-gossip-app: simple p2p gossiping cli application to send messages to peers every N seconds.
---
+++ rust-examples/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // Program 1 - hello world
+    println!("Program 1 - Hello, world!");
+
+    // Program 2 - Variables
+    let x: i32 = 5;
+    let _y: i32; // uninitialized variable
+
+    assert_eq!(x, 5);
+    println!("Program 2 - Success!")
+
+    // Program 3 - Mutuable variable
+
+    // todo: Scope, Function, Shadowing
+}
